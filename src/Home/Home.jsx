8f917d970ddcc42c0c87036fe86bd29d3e7555a6
Muhammad-RK-Isa <muhammad-rk-isa@outlook.com>
2023-04-11 06:03:53 +0600

    initialized project

---
+++ src/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className='text-6xl text-blue-600 font-extrabold'>
+            This is the homepage.....
+        </div>
+    );
+};
+
+export default Home;
